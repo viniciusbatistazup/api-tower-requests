e37151a4e029198d842574e57c441b169fd16712
vinicius.batista.zup <vinicius.batista@zup.com.br>
2021-08-31 07:39:01 -0300

    Add wsgi

--- main-tower/app.py
+++ main-tower/app.py
@@ -1,13 +1,20 @@
 # encoding: utf-8
 import json
+import logging
+import socket
 from flask import Flask, request
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import make_wsgi_app
 from prometheus_flask_exporter import PrometheusMetrics
-
+# from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
 
 app = Flask(__name__)
 
 metrics = PrometheusMetrics(app)
 
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
 
 @app.route('/200')
 def success():
@@ -39,4 +46,15 @@ def hello():
     return json.dumps({'msg': 'Hello !'}), 200
 
 
-app.run()
+@app.route('/debug')
+def debug():
+    logging.debug(' debug')
+    logging.info(' info')
+    logging.warning(' warning')
+    logging.error(' error')
+    logging.critical(' critical')
+    return json.dumps({'msg': 'Debug !'}), 200
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
